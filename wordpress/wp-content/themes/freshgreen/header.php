30f742c135d7ec10a8589539e2848928a95b82ce
tofishes <tofishes@163.com>
2014-12-16 15:44:47 +0800

    update files

--- wordpress/wp-content/themes/freshgreen/header.php
+++ wordpress/wp-content/themes/freshgreen/header.php
@@ -27,7 +27,7 @@
 <link rel="profile" href="http://cssor.com/about" />
 <link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url')?>/img-res/favicon.ico" />
 <link rel="icon" type="image/png" href="<?php bloginfo('template_url')?>/img-res/favicon.png" />
-<link rel="stylesheet" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="stylesheet" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>?v1" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lte IE 8]>
 <script>(function(){
@@ -53,19 +53,6 @@
 		get_css_meta();
 	}
 ?>
-<!-- toDel -->
-<script>
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-17953327-2']);
-  _gaq.push(['_setDomainName', 'cssor.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
 </head>
 
 <body <?php body_class(); ?>>
